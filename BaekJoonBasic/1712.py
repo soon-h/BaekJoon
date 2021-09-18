c527e095de441dd4af8ffd884b29da00f77d6809
soon-h <ksh8895420@naver.com>
2021-09-18 22:04:03 +0900

    Day7
    파이썬을 이용한 기본 수학1 문제 풀이

--- BaekJoonBasic/1712.py
+++ BaekJoonBasic/1712.py
@@ -1,16 +1,6 @@
-from typing import Counter
-
-
 A , B , C = map(int,input().split())
-i = 1
 
-while True:
-    if B >= C:
-        print("-1")
-        break
-    
-    if A + (B * i) == C * i:
-        print(i + 1)
-        break
-    else:
-        i += 1
+if B>=C:
+    print(-1)
+else:
+    print(int(A/(C-B)+1))
---
+++ BaekJoonBasic/2292.py
@@ -0,0 +1,8 @@
+arrivepoint = int(input())
+compare = 1
+addvalue = 6
+count = 1
+while arrivepoint > compare:
+    compare += (addvalue * count)
+    count += 1
+print(count)
